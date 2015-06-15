a55bb486c51e612b9774f5b6859d35a32cf32d1e
OTSUKA Tatsuya <tatzyr@gmail.com>
2015-06-15 23:52:37 +0900

    Switch from minitest to RSpec

---
+++ spec/coral_backup_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe CoralBackup do
+  it 'has a version number' do
+    expect(CoralBackup::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,2 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'coral_backup'
-
-require 'minitest/autorun'
