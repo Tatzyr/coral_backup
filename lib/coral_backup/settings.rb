f39a87c7261373aa8904a7b21148c115167b8835
Tatsuya Otsuka <tatzyr@gmail.com>
2015-09-26 19:53:44 +0900

    Change method name to load!/save!

--- lib/coral_backup/settings.rb
+++ lib/coral_backup/settings.rb
@@ -8,7 +8,7 @@ module CoralBackup
 
     def initialize(filename = "#{Dir.home}/.coral_backup")
       @filename = filename
-      @settings = file_load
+      @settings = load!
     end
 
     def action_data(action_name)
@@ -27,29 +27,28 @@ module CoralBackup
     def add(action_name, source, destination, excluded_files)
       raise ArgumentError, "Backup action `#{action_name}' already exists." if exist_action?(action_name)
       @settings[:actions][action_name] = { source: source, destination: destination, excluded_files: excluded_files }
-      file_dump
+      save!
     end
 
     def delete(action_name)
       raise ArgumentError, "Backup action `#{action_name}' does not exist." unless exist_action?(action_name)
       @settings[:actions].delete(action_name)
-      file_dump
+      save!
     end
 
     def update_time(action_name, time)
       raise ArgumentError, "Backup action `#{action_name}' does not exist." unless exist_action?(action_name)
       @settings[:actions][action_name][:last_excuted_at] = time.to_s
-      file_dump
+      save!
     end
 
     private
-    def file_load
+    def load!
       return INITIAL_SETTINGS unless FileTest.exist?(@filename)
-
       YAML.load_file(@filename)
     end
 
-    def file_dump
+    def save!
       open(@filename, "w") do |f|
         YAML.dump(@settings, f)
       end
